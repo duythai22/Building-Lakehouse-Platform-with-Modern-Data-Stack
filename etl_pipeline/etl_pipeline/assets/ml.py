dda38af603b4f43bff2a0ed61d07225401d03772
ferr2 <thaiduy756@gmail.com>
2024-03-13 16:48:46 +0700

    update etl_pipeline

--- etl_pipeline/etl_pipeline/assets/ml.py
+++ etl_pipeline/etl_pipeline/assets/ml.py
@@ -140,116 +140,14 @@ def extract(
 #         )
 
 
-# genre from my_sql
-@asset(
-    description="Train model test",
-    key_prefix=["ml", "customer"],
-    compute_kind=COMPUTE_KIND,
-    group_name=LAYER,
-)
-
-def run():
-    warnings.filterwarnings("ignore")
-    np.random.seed(40)
-
-    # Read the wine-quality csv file from the URL
-    csv_url = (
-        "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-    )
-    try:
-        data = pd.read_csv(csv_url, sep=";")
-    except Exception as e:
-        logger.exception(
-            "Unable to download training & test CSV, check your internet connection. Error: %s", e
-        )
-
-    # Split the data into training and test sets. (0.75, 0.25) split.
-    train, test = train_test_split(data)
-
-    # The predicted column is "quality" which is a scalar from [3, 9]
-    train_x = train.drop(["quality"], axis=1)
-    test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
-
-    #alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    #l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
-    mlflow.set_tracking_uri("http://mlflow_server:5000")
-    experiment_name = "experiment_01032024"
-    mlflow.set_experiment(experiment_name)
-
-    # Khởi tạo MLfow
-    with mlflow.start_run():
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
-    
-        predicted_qualities = lr.predict(test_x)
-
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
-
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
-        logging.debug("Metrics logged")
-
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-        logging.debug(f'Tracking URL: {tracking_url_type_store}')
-
-
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
-
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(lr, "sk_models", registered_model_name="ElasticnetWineModel")
-        else:
-            # Ghi lại mô hình
-            mlflow.sklearn.log_model(lr, "sk_models")
-    print("Model training complete")
-    return True
-
-# Extract data từ mysql
-# def bronze_customer(context) -> Output[pl.DataFrame]:
-#     query = "SELECT * FROM customers;"
-#     df_data = context.resources.mysql_io_manager.extract_data(query)
-#     context.log.info(f"Table extracted with shape: {df_data.shape}")
-
-#     return Output(
-#         value=df_data,
-#         metadata={
-#             "table": "customers",
-#             "row_count": df_data.shape[0],
-#             "column_count": df_data.shape[1],
-#             "columns": df_data.columns,
-#         },
-#     )
-
-# COMPUTE_KIND = "Mlflow"
-# LAYER = "machinelearning"
-
-# def eval_metrics(actual, pred):
-#     rmse = np.sqrt(mean_squared_error(actual, pred))
-#     mae = mean_absolute_error(actual, pred)
-#     r2 = r2_score(actual, pred)
-#     return rmse, mae, r2
-
-
+# # genre from my_sql
 # @asset(
-#     description="mlflows",
-#     key_prefix=["machinelearning", "linear"],
+#     description="Train model test",
+#     key_prefix=["ml", "customer"],
 #     compute_kind=COMPUTE_KIND,
 #     group_name=LAYER,
 # )
+
 # def run():
 #     warnings.filterwarnings("ignore")
 #     np.random.seed(40)
@@ -277,11 +175,15 @@ def run():
 #     #alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
 #     #l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
+#     mlflow.set_tracking_uri("http://mlflow_server:5000")
+#     experiment_name = "experiment_01032024"
+#     mlflow.set_experiment(experiment_name)
+
 #     # Khởi tạo MLfow
 #     with mlflow.start_run():
 #         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
 #         lr.fit(train_x, train_y)
-
+    
 #         predicted_qualities = lr.predict(test_x)
 
 #         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
@@ -315,3 +217,5 @@ def run():
 #             mlflow.sklearn.log_model(lr, "sk_models")
 #     print("Model training complete")
 #     return True
+
+
